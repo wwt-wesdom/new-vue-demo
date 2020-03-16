dc3ee31851fc0d73b8f596832aaf26ef0f8e1de9
wangwentao <wangwentao@zhibitech.com>
2020-03-16 19:50:41 +0800

    checkBox

---
+++ src/components/checkBox/CheckBoxGroup.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "CheckBoxGroup"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/UseComponent.vue
+++ src/views/UseComponent.vue
@@ -4,6 +4,9 @@
         <c-button ref="cButton" class="mt-20" @click.native="submit">确定</c-button>
         <provide class="mt-20"></provide>
         <div class="mt-20" @click="changePhone">{{app.userInfo ? app.userInfo.phone : ''}}</div>
+        <div class="mt-30">
+            <check-box>选择框</check-box>
+        </div>
     </div>
 </template>
 
@@ -19,6 +22,8 @@
             // const child = this.$refs.cButton;
             // console.log(child.title);
             // child.sayHi('xiaoming')
+            let arr = [1,1,2,2,3,3,3,4,4,5,6,7,7];
+            console.log(this.arrUnique(arr));
         },
         methods: {
             submit(e) {
@@ -26,6 +31,15 @@
             },
             changePhone() {
                 this.app.getUserInfo();
+            },
+            arrUnique(arr) {
+                let newArr = [];
+                for (let i = 0; i < arr.length; i++) {
+                    if (arr[i] !== arr[i+1]) {
+                        newArr.push(arr[i])
+                    }
+                }
+                return newArr;
             }
         }
     }
