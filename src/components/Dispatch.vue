b9e8b8ed624683a2047c323c684553833d08299e
wangwentao <123456Wwt>
2020-05-06 08:51:58 +0800

    v-model的语法糖使用

---
+++ src/components/Dispatch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <c-button @click.native="clickHandle">dispatch</c-button>
+  </div>
+</template>
+
+<script>
+  import emitter from "../mixins/emitter";
+  export default {
+    name: "Dispatch",
+    mixins: [emitter],
+    mounted() {
+      this.$on('parent-broadcast', function (text) {
+        console.log(text + ' ' +  this.$options.name);
+      })
+    },
+    methods: {
+      clickHandle() {
+        this.dispatch('UseComponent', 'test','hello vue.js')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
